e3962e700f3e540e4e02eb12cfb96eace20f3567
pkc1088 <pkcmax@naver.com>
2025-07-17 04:38:49 +0900

    policy/terms updated, scheduledTask set to all users

--- src/main/java/youtube/youtubeService/controller/ScheduledTaskController.java
+++ src/main/java/youtube/youtubeService/controller/ScheduledTaskController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import youtube.youtubeService.domain.Playlists;
+import youtube.youtubeService.domain.Users;
 import youtube.youtubeService.policy.SearchPolicy;
+import youtube.youtubeService.repository.users.UserRepository;
 import youtube.youtubeService.service.playlists.PlaylistService;
 import youtube.youtubeService.service.users.UserService;
 import youtube.youtubeService.service.youtube.YoutubeService;
@@ -30,15 +32,17 @@ public class ScheduledTaskController {
     private final YoutubeService youtubeService;
     private final UserService userService;
     private final SearchPolicy searchPolicy;
+    private final UserRepository userRepository;
 
     @Autowired
     public ScheduledTaskController( // @Qualifier("simpleSearchQuery")
                                 PlaylistService playlistService, YoutubeService youtubeService, UserService userService,
-                                @Qualifier("geminiSearchQuery") SearchPolicy searchPolicy) {
+                                @Qualifier("geminiSearchQuery") SearchPolicy searchPolicy, UserRepository userRepository) {
         this.playlistService = playlistService;
         this.youtubeService = youtubeService;
         this.userService = userService;
         this.searchPolicy = searchPolicy;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/track-recovery")
@@ -47,12 +51,13 @@ public class ScheduledTaskController {
         if (apiKey == null || !apiKey.equals(this.apiKey)) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid API Key");
         }
-        allPlaylistsRecoveryOfOneParticularUserTest();
+//        allPlaylistsRecoveryOfOneParticularUser();
+        allPlaylistsRecoveryOfAllUsers();
         log.info("track-recovery endpoint done");
         return ResponseEntity.ok("Auto Track&Recovery Task executed");
     }
 
-    public void allPlaylistsRecoveryOfOneParticularUserTest() throws IOException {
+    public void allPlaylistsRecoveryOfOneParticularUser() throws IOException {
         log.info("auto scheduler activated");
         // 0. 전체 유저 목록에서 순차적으로 유저를 뽑아 오는 시나리오 있다 치고
         String userId  = "112735690496635663877";
@@ -62,7 +67,6 @@ public class ScheduledTaskController {
         List<Playlists> playListsSet = playlistService.getPlaylistsByUserId(userId);
         for (Playlists playlist : playListsSet) {
             log.info("{} start", playlist.getPlaylistTitle());
-            // playlist 자체가 제거된 경우 예외처리 필요
             try {
                 youtubeService.fileTrackAndRecover(userId, playlist.getPlaylistId(), accessToken);
             } catch (IOException e) {
@@ -71,6 +75,29 @@ public class ScheduledTaskController {
         }
 
         log.info("auto scheduler done");
+    }
+
+    public void allPlaylistsRecoveryOfAllUsers() throws IOException {
+        // 0. 전체 유저 목록에서 순차적으로 유저를 뽑기
+        List<Users> users = userRepository.findAllUsers();
+
+        for (Users user : users) {
+            String userId = user.getUserId(); // String userId  = "112735690496635663877";
+            log.info("userId : {}", userId);
+            // 1. 유저 아이디로 accessToken 발급
+            String accessToken = userService.getNewAccessTokenByUserId(userId);
+            // 2. 유저 아이디로 조회한 모든 플레이리스트 & 음악을 디비에서 뽑아서 복구 시스템 가동
+            List<Playlists> playListsSet = playlistService.getPlaylistsByUserId(userId);
+            for (Playlists playlist : playListsSet) {
+                log.info("{} start", playlist.getPlaylistTitle());
+                try {
+                    youtubeService.fileTrackAndRecover(userId, playlist.getPlaylistId(), accessToken);
+                } catch (IOException e) {
+                    log.info("scheduler caught and then move to next playlist");
+                }
+            }
+
+        }
     }
 
 }
